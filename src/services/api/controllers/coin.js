bb53a08185ee209ed7a68aeb219f2aa17b0a7acb
Mohamed Emad <mohamed.emad4bubble@gmail.com>
2022-03-25 16:49:34 +0200

    query for price and cache the results mechanism;dont allow users to create coins that coingecko doesnt recognize

--- src/services/api/controllers/coin.js
+++ src/services/api/controllers/coin.js
@@ -1,12 +1,18 @@
 const errors = require('../../../helpers/errors');
 const Models = require('../../../models/pg');
+const { fetchCoinPrice, getCoinGeckoIDfromCoinCode } = require('../../../helpers/coingecko-api');
 
 const CoinController = {
   async getCoinByCode(coinCode) {
-    const coin = await Models.Coin.findByCoinCode(coinCode);
+    let coin = await Models.Coin.findByCoinCode(coinCode);
 
     errors.assertExposable(coin, 'unknown_coin_code');
 
+    // if the last update was an hour ago check coingecko for an update
+    if (coin.priceLastUpdated < new Date() - 1000 * 60 * 60 || !coin.priceLastUpdated) {
+      const price = await fetchCoinPrice(coin.coinGeckoID);
+      if (price) coin = await Models.Coin.updateCoinPrice(coinCode, price);
+    }
     return coin.filterKeys();
   },
 
@@ -14,7 +20,10 @@ const CoinController = {
     let coin = await Models.Coin.findByCoinCode(coinCode);
     errors.assertExposable(!coin, 'coin_already_exists');
 
-    coin = await Models.Coin.createCoin(name, coinCode);
+    const coinGeckoID = await getCoinGeckoIDfromCoinCode(coinCode);
+    errors.assertExposable(coinGeckoID, 'invalid_coin_code'); // throw an error if coinGecko doesn't know the coin
+
+    coin = await Models.Coin.createCoin(name, coinCode, coinGeckoID);
     return coin.filterKeys();
   },
 };
--- test/unit/services/api/controllers/coin.spec.js
+++ test/unit/services/api/controllers/coin.spec.js
@@ -3,6 +3,7 @@ const sinon = require('sinon');
 const sequelizeMockingMocha = require('sequelize-mocking').sequelizeMockingMocha;
 const CoinController = require(path.join(srcDir, '/services/api/controllers/coin'));
 const DB = require(path.join(srcDir, 'modules/db'));
+const { mockCoingecko, unmockCoingecko } = require('../../../../mocks/coingecko-nock')
 
 describe('Controller: Coin', () => {
   let sandbox = null;
@@ -10,10 +11,12 @@ describe('Controller: Coin', () => {
   sequelizeMockingMocha(DB.sequelize, [path.resolve('test/mocks/coins.json')], { logging: false });
 
   beforeEach(async () => {
+    mockCoingecko()
     sandbox = sinon.createSandbox();
   });
 
   afterEach(() => {
+    unmockCoingecko()
     sandbox && sandbox.restore();
   });
 
@@ -23,9 +26,27 @@ describe('Controller: Coin', () => {
       const coin = await CoinController.getCoinByCode(coinCode);
 
       expect(coin.code).to.eq(coinCode);
-      expect(Object.keys(coin).length).to.eq(3);
+      expect(Object.keys(coin).length).to.eq(5);
     });
 
+    it('should update coin price if an hour passes', async () => {
+      const coinCode = 'BTC';
+      await CoinController.getCoinByCode(coinCode); // fetch current price
+
+
+      sinon.useFakeTimers(new Date(+new Date() + 60 * 1000 * 30).getTime()); // advance clock by 30m
+      let coin2 = await CoinController.getCoinByCode(coinCode);
+      // check if priceLastUpdated is approx 30m ago
+      expect(coin2.priceLastUpdated.valueOf()).to.approximately(new Date(+new Date() - 60 * 1000 * 30).valueOf(), 1000);
+
+
+      clock = sinon.useFakeTimers(new Date(+new Date() + 60 * 1000 * 31).getTime()); // advance clock by 31m
+      let coin3 = await CoinController.getCoinByCode(coinCode);
+      // check if the priceLastUpdated is now
+      expect(coin3.priceLastUpdated.valueOf()).to.approximately(new Date().valueOf(), 1000);
+
+      clock.restore();
+    });
     it('should fail get coin by code', async () => {
       const coinCode = 'AMN';
       expect(CoinController.getCoinByCode(coinCode)).to.be.rejectedWith(Error, 'unknown_coin_code');
@@ -45,6 +66,10 @@ describe('Controller: Coin', () => {
     it('should refuse creating the same coin if it exists', async () => {
       await CoinController.createCoin(coinName, coinCode);
       expect(CoinController.createCoin(coinName, coinCode)).to.be.rejectedWith(Error, 'coin_already_exists');
+    });
+
+    it('should refuse creating a coin with an invalid coinCode', () => {
+      expect(CoinController.createCoin(coinName, 'fakecoin')).to.be.rejectedWith(Error, 'invalid_coin_code');
     });
   });
 });
